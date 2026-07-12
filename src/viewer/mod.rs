e1f4d87a202a087d57c871e595c75b1be866af00
Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>
2026-07-11 21:13:09 -0700

    Extract scene viewer renderer

--- src/viewer/mod.rs
+++ src/viewer/mod.rs
@@ -1,5 +1,11 @@
 mod input;
+mod scene_renderer;
 mod state;
 
 pub use state::ViewerState;
 pub use input::{handle_key, ViewerInput};
+
+pub use scene_renderer::{
+    draw_render_scene, load_scene_maps, load_scene_meshes, read_scene, VIEW_SCENE_HEIGHT,
+    VIEW_SCENE_WIDTH,
+};
